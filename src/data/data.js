5967980e57285bebfb4e5250dfee3460bc6c36b6
CodePawsDev <isripen1991@outlook.com>
2023-08-07 22:51:27 +0700

    feat: add preview card page

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+import SedansIcon from "../assets/images/icon-sedans.svg";
+import SUVSIcon from "../assets/images/icon-suvs.svg";
+import LuxuryIcon from "../assets/images/icon-luxury.svg";
+
+export const data = [
+{   title: "SEDANS",
+    content: "Choose a sedan for its affordabilitty and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    bgColor: "bg-orange-400",
+    textColor: "text-orange-400",
+    img: SedansIcon,
+},
+{
+    title: "SUVS",
+    content: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    bgColor: "bg-teal-700",
+    textColor: "text-teal-700",
+    img: SUVSIcon,
+},
+{
+    title: "LUXURY",
+    content: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    bgColor: "bg-teal-900",
+    textColor: "text-teal-900",
+    img: LuxuryIcon,
+},
+]
